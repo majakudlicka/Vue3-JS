22c27df5a7ec2be8c2b6867b7dfaff9ab0db3021
Maja Kudlicka <maja.kudlicka@gmail.com>
2021-05-27 07:36:07 +0200

    refactor: create composable use-keydown function

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -3,7 +3,7 @@
 		<table class="mail-table">
 			<tbody>
 			<tr v-for="email in unarchivedEmails" :key="email.id" :class="['clickable', email.read ? 'read' : '']"
-					@click="readEmail(email)">
+					@click="openEmail(email)">
 				<td>
 					<input type="checkbox"/>
 				</td>
@@ -18,6 +18,12 @@
 			</tr>
 			</tbody>
 		</table>
+		<ModalView v-if="openedEmail" @closeModal="openedEmail = null">
+		<MailView :email="openedEmail"></MailView>
+		</ModalView>
+		<div v-if="openedEmail">
+			{{ openedEmail.subject }}
+		</div>
 	</div>
 </template>
 
@@ -25,6 +31,8 @@
 import {format} from "date-fns";
 import {ref} from 'vue';
 import axios from 'axios';
+import MailView from "@/components/MailView";
+import ModalView from "@/components/ModalView";
 
 export default {
 	// name: 'MailTable',
@@ -32,8 +40,13 @@ export default {
 		let { data: emails } = await axios.get('http://localhost:3000/emails');
 		return {
 			format,
-			emails
+			emails,
+			openedEmail: null
 		}
+	},
+	components: {
+		MailView,
+		ModalView
 	},
 	computed: {
 		sortedEmails() {
@@ -45,9 +58,10 @@ export default {
 			return this.sortedEmails.filter(e => !e.archived);
 		}
 	}, methods: {
-		readEmail(email) {
+		openEmail(email) {
 			email.read = true;
 			this.updateEmail(email);
+			this.openedEmail = email;
 		},
 		archiveEmail(email) {
 			email.archived = true;
