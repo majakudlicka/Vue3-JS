322a3fcc8cd5145b02015fa0f0296ee06679639d
Maja Kudlicka <maja.kudlicka@gmail.com>
2021-06-06 10:52:33 +0200

    chore: demo app contd

---
+++ src/composables/use-email-selection.ts
@@ -0,0 +1,59 @@
+import { reactive } from "vue";
+import axios from "axios";
+import { IEmail } from "@/types/email";
+
+const emailSet = new Set();
+
+export const useEmailSelection = function () {
+  const emails = reactive(emailSet);
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const forSelected = (fn: Function) => {
+    emails.forEach((email: any) => {
+      fn(email);
+      axios.put(`http://localhost:3000/emails/${email.id}`, email);
+    });
+  };
+  const clear = () => {
+    emails.clear();
+  };
+  const toggle = (id: string) => {
+    if (emails.has(id)) {
+      emails.delete(id);
+    } else {
+      emails.add(id);
+    }
+  };
+  const addMultiple = (newEmails: IEmail[]) => {
+    newEmails.forEach((email) => {
+      emails.add(email);
+    });
+  };
+  const markRead = () => {
+    forSelected((e: IEmail) => (e.read = true));
+  };
+  const markUnread = () => {
+    forSelected((e: IEmail) => (e.read = false));
+  };
+  const archive = () => {
+    forSelected((e: IEmail) => (e.archived = true));
+    clear();
+  };
+  const moveToInbox = () => {
+    forSelected((e: IEmail) => (e.archived = false));
+    clear();
+  };
+
+  return {
+    emails,
+    clear,
+    toggle,
+    addMultiple,
+    markRead,
+    markUnread,
+    archive,
+    moveToInbox,
+  };
+};
+
+export default useEmailSelection;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <Suspense>
+      <template #default>
+        <MailScreen />
+      </template>
+      <template #fallback> Loading... </template>
+    </Suspense>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import MailScreen from "../components/MailScreen.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    MailScreen,
   },
 });
 </script>
